9cd49f8a662bdf6bc68b9a72ca83d6c19ad34544
guentherKI <paul.gessner@gmx.net>
2025-10-10 16:08:22 +0200

    Update Chats.jsx

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -69,7 +69,7 @@ export default function Chats({ profile }) {
           <strong>Deine ID:</strong> <code>{peerId}</code>
         </div>
         <div>
-          Gib die ID deines Chatpartners ein und klicke auf<b>Verbinden</b>.
+          Gib die ID deines Chatpartners ein und klicke auf <b>Verbinden</b>.
         </div>
       </div>
       <div className="start-chat">
