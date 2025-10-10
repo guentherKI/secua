11b400f864409ee15c0b69019d818bf7e0f31dd9
guentherKI <paul.gessner@gmx.net>
2025-10-10 16:32:05 +0200

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Settings from "./Settings";
 import Chats from "./Chats";
 import Contacts from "./Contacts";
 import History from "./History";
+import Settings from "./Settings";
 
 export default function Dashboard({ currentPage, profile, setProfile }) {
-  if (currentPage === "chats") return <Chats profile={profile} />;
-  if (currentPage === "contacts") return <Contacts />;
-  if (currentPage === "history") return <History />;
-  if (currentPage === "settings")
-    return <Settings profile={profile} setProfile={setProfile} />;
-  return null;
+  return (
+    <>
+      {currentPage === "chats" && <Chats profile={profile} setProfile={setProfile} />}
+      {currentPage === "contacts" && <Contacts />}
+      {currentPage === "history" && <History />}
+      {currentPage === "settings" && <Settings />}
+    </>
+  );
 }
